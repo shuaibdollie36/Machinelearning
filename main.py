eb9c92f90f93eb634b600f32e5a19d87660616b2
Shuaibdollie36 <shuaibdollie36@gmail.com>
2021-06-01 14:06:07 +0200

    added new file

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+x = np.arange(21)
+print("\nOriginal array:")
+print(x)
+r1 = np.mean(x)
+r2 = np.average(x)
+assert np.allclose(r1, r2)
+print("\nMean: ", r1)
+r1 = np.std(x)
+r2 = np.sqrt(np.mean((x - np.mean(x)) ** 2 ))
+assert np.allclose(r1, r2)
+print("\nstd: ", r1)
+r1= np.var(x)
+r2 = np.mean((x - np.mean(x)) ** 2 )
+assert np.allclose(r1, r2)
+print("\nvariance: ", r1)
